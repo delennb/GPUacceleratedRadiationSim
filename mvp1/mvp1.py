9890f88667c7fe3a1c0bd06cd51533dd2051e294
root <mathrulespi@gmail.com>
2024-10-07 10:59:17 -0400

    Initial Commit

---
+++ mvp1/mvp1.py
@@ -0,0 +1,18 @@
+import math
+
+def main():
+    # Add code for project here
+
+    # Lambert's Law - I = I0 * e^(-2)
+    #   z = optical depth
+    #   z = (0, inf)
+    # CPU Version & GPU Version
+    # Many layers - n layers
+    # z = integral(dz)
+    # Use prefix-sum
+
+    print('radiation yay')
+
+
+if __name__ == "__main__":
+    main()
